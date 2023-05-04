11533972648755d641aab20d6469f1e12c03b274
gergoszaszvaradi <szaszvaradi.gergo@yahoo.com>
2023-05-04 08:39:11 +0300

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,50 @@
-/**
- * Welcome to Cloudflare Workers! This is your first scheduled worker.
- *
- * - Run `wrangler dev --local` in your terminal to start a development server
- * - Run `curl "http://localhost:8787/cdn-cgi/mf/scheduled"` to trigger the scheduled event
- * - Go back to the console to see what your worker has logged
- * - Update the Cron trigger in wrangler.toml (see https://developers.cloudflare.com/workers/wrangler/configuration/#triggers)
- * - Run `wrangler publish --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/runtime-apis/scheduled-event/
- */
-
-export interface Env {
+interface Env {
     db : D1Database;
     boards : R2Bucket;
 }
 
-export interface Board {
+interface Board {
     id : string;
     isPermanent : boolean;
     modifiedAt : number;
 }
 
+async function cleanUp(env : Env) : Promise<void> {
+    const boards = await env.db.prepare("SELECT * FROM boards WHERE modifiedAt <= date('now', '-7 day') AND isPermanent = FALSE").all<Board>();
+    console.log(boards);
+    if (!boards || !boards.success || !boards.results) {
+        console.error("Error while reading database:", boards.error);
+        throw new Error(`Error while reading database: ${boards.error}`);
+    }
+
+    const ids = boards.results.map((board) => board.id);
+
+    console.log("Deleting boards:", ids);
+
+    const result = await env.db.prepare(`DELETE FROM boards WHERE id IN (${boards.results.map((board) => `'${board.id}'`).join(",")})`).run();
+    console.log(result);
+    if (!result || !result.success || !result.results) {
+        console.error("Error while deleting boards from database:", result.error);
+        throw new Error(`Error while deleting boards from database: ${result.error}`);
+    }
+
+    console.log("Deleting boards contents:", ids);
+    await env.boards.delete(ids);
+
+    console.log("Successfully deleted boards");
+}
+
 export default {
     async scheduled(controller : ScheduledController, env : Env, ctx : ExecutionContext) : Promise<void> {
+        return cleanUp(env);
+    },
+    async fetch(request : Request, env : Env) : Promise<Response> {
         try {
-            const boards = await env.db.exec<Board>("SELECT * FROM boards WHERE modifiedAt <= date('now', '-7 day') AND isPermanent = FALSE");
-            if (!boards || !boards.success || !boards.results) {
-                console.error("Error while reading database:", boards.error);
-                return;
-            }
-
-            const ids = boards.results.map((board) => board.id);
-
-            console.log("Deleting boards:", ids);
-
-            const result = await env.db.exec(`DELETE FROM boards WHERE id IN (${boards.results.map((board) => `'${board.id}'`).join(",")})`);
-            if (!result || !result.success) {
-                console.error("Error while deleting boards from database:", result.error);
-                return;
-            }
-
-            console.log("Deleting boards contents:", ids);
-            await env.boards.delete(ids);
-
-            console.log("Successfully deleted boards");
+            await cleanUp(env);
+            return new Response("ok", { status: 200 });
         } catch (e) {
-            console.error(e);
+            console.error(e as string);
+            return new Response(e as string, { status: 500 });
         }
     },
 };
